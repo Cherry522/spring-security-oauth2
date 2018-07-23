e51b50bb4edf0c388b1a7456c7ca6edfc9286509
Cherry522 <522581280@qq.com>
2018-07-23 17:19:35 +0800

    添加注释

--- src/main/java/com/cherry/demo/springsecurityoauth2/OAuth2Configuration.java
+++ src/main/java/com/cherry/demo/springsecurityoauth2/OAuth2Configuration.java
@@ -99,19 +99,45 @@ public class OAuth2Configuration {
             return new JdbcTokenStore(dataSource);
         }
 
+        /**
+         * 认证管理器，当你选择了资源所有者密码（password）授权类型的时候，请设置这个属性注入一个 AuthenticationManager 对象。
+         */
         @Autowired
         @Qualifier("authenticationManager")
         private AuthenticationManager authenticationManager;
 
         /**
          * 用来配置授权（authorization）以及令牌（token）的访问端点和令牌服务(token services)。
+         * 配置授权类型（Grant Types）：
+         * 授权是使用 AuthorizationEndpoint 这个端点来进行控制的，你能够使用 AuthorizationServerEndpointsConfigurer 这个对象的实例来进行配置(AuthorizationServerConfigurer 的一个回调配置项，见上的概述) ，如果你不进行设置的话，默认是除了资源所有者密码（password）授权类型以外，支持其余所有标准授权类型的（RFC6749），我们来看一下这个配置对象有哪些属性可以设置吧，如下列表：
+         * authenticationManager：认证管理器，当你选择了资源所有者密码（password）授权类型的时候，请设置这个属性注入一个 AuthenticationManager 对象。
+         * userDetailsService：如果啊，你设置了这个属性的话，那说明你有一个自己的 UserDetailsService 接口的实现，或者你可以把这个东西设置到全局域上面去（例如 GlobalAuthenticationManagerConfigurer 这个配置对象），当你设置了这个之后，那么 "refresh_token" 即刷新令牌授权类型模式的流程中就会包含一个检查，用来确保这个账号是否仍然有效，假如说你禁用了这个账户的话。
+         * authorizationCodeServices：这个属性是用来设置授权码服务的（即 AuthorizationCodeServices 的实例对象），主要用于 "authorization_code" 授权码类型模式。
+         * implicitGrantService：这个属性用于设置隐式授权模式，用来管理隐式授权模式的状态。
+         * tokenGranter：这个属性就很牛B了，当你设置了这个东西（即 TokenGranter 接口实现），那么授权将会交由你来完全掌控，并且会忽略掉上面的这几个属性，这个属性一般是用作拓展用途的，即标准的四种授权模式已经满足不了你的需求的时候，才会考虑使用这个。
+         *
+         * 在XML配置中呢，你可以使用 "authorization-server" 这个标签元素来进行设置。
          * @param endpoints
          * @throws Exception
          */
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
             endpoints.tokenStore(tokenStore())
-                    .authenticationManager(authenticationManager);
+                    //认证管理器，当你选择了资源所有者密码（password）授权类型的时候，请设置这个属性注入一个 AuthenticationManager 对象。
+                    .authenticationManager(authenticationManager)
+                    //配置授权端点的URL（Endpoint URLs）：
+                    //AuthorizationServerEndpointsConfigurer 这个配置对象(AuthorizationServerConfigurer 的一个回调配置项，见上的概述) 有一个叫做 pathMapping() 的方法用来配置端点URL链接，它有两个参数：
+                    //第一个参数：String 类型的，这个端点URL的默认链接。
+                    //第二个参数：String 类型的，你要进行替代的URL链接。
+                    //以上的参数都将以 "/" 字符为开始的字符串，框架的默认URL链接如下列表，可以作为这个 pathMapping() 方法的第一个参数：
+                    ///oauth/authorize：授权端点。
+                    ///oauth/token：令牌端点。
+                    ///oauth/confirm_access：用户确认授权提交端点。
+                    ///oauth/error：授权服务错误信息端点。
+                    ///oauth/check_token：用于资源服务访问的令牌解析端点。
+                    ///oauth/token_key：提供公有密匙的端点，如果你使用JWT令牌的话。
+//                    .pathMapping("/oauth/authorize","/oauth/aa")
+            ;
         }
 
         /**
@@ -145,6 +171,12 @@ public class OAuth2Configuration {
                     .withClient(propertyResolver.getProperty(PROP_CLIENTID))
                     .scopes("read", "write")
                     .authorities(Authorities.ROLE_ADMIN.name(), Authorities.ROLE_USER.name())
+                    //authorizedGrantTypes可以取值：
+                    //authorization_code：授权码类型。
+                    //implicit：隐式授权类型。
+                    //password：资源所有者（即用户）密码类型。
+                    //client_credentials：客户端凭据（客户端ID以及Key）类型。
+                    //refresh_token：通过以上授权获得的刷新令牌来获取新的令牌。
                     .authorizedGrantTypes("password", "refresh_token")
                     .secret(propertyResolver.getProperty(PROP_SECRET))
                     .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
